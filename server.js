98e1e6be2e73c874b05881e9942c9814b88e959e
candice <candiceschramm@gmail.com>
2019-05-18 18:02:20 -0600

    installed setup morgan to log requets, installed mongoose npm package and set up connection to db

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
+var mongoose = require("mongoose");
+var logger = require("morgan");
 
 const app = express();
 const PORT = 3000;
@@ -8,6 +10,8 @@ const PORT = 3000;
 
 //body-parser set up
 app.use(bodyParser.urlencoded({extended: false}));
+//set up morgan to log http requests
+app.use(logger("dev"));
 
 //public files
 app.use(express.static("public"));
@@ -24,6 +28,8 @@ app.set("view engine", "handlebars")
 //routes
 require("./routes/htmlRoutes")(app);
 require("./routes/apiRoutes")(app);
+
+mongoose.connect("mongodb://localhost/natGeoScraper", {useNewUrlParser: true})
 
 
 app.listen(PORT, () => {
